b2b5a9577d91855b9429010c335f7225a158476e
Kirill Zaverukha <zaveruha678@gmail.com>
2024-08-28 20:56:01 +0300

    Added base plugin files

---
+++ Source/MvAbilitySystem/MvAbilitySystem.Build.cs
@@ -0,0 +1,49 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class MvAbilitySystem : ModuleRules
+{
+	public MvAbilitySystem(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		
+		PublicIncludePaths.AddRange(
+			new string[] {
+			}
+			);
+				
+		
+		PrivateIncludePaths.AddRange(
+			new string[] {
+			}
+			);
+			
+		
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				"CoreUObject",
+				"Engine",
+				"Slate",
+				"SlateCore",
+				"GameplayAbilities",
+				"GameplayTags",
+				"GameplayTasks"
+			}
+            );
+			
+		
+		PrivateDependencyModuleNames.AddRange(
+			new string[] {
+			}
+			);
+		
+		
+		DynamicallyLoadedModuleNames.AddRange(
+			new string[] {
+			}
+			);
+	}
+}
