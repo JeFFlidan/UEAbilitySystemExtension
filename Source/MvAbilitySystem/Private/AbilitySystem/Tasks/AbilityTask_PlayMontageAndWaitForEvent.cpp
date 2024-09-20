be3ecacce5d78aff7b8dd62c13ebac64a7be22e0
JeFFlidan <zaveruha678@gmail.com>
2024-09-20 23:08:05 +0300

    Added PlayMontageAndWaitForEvent ability task

---
+++ Source/MvAbilitySystem/Private/AbilitySystem/Tasks/AbilityTask_PlayMontageAndWaitForEvent.cpp
@@ -0,0 +1,250 @@
+﻿// Copyright Kyrylo Zaverukha. All Rights Reserved.
+
+#include "AbilitySystem/Tasks/AbilityTask_PlayMontageAndWaitForEvent.h"
+#include "AbilitySystem/Abilities/MvGameplayAbility.h"
+#include "MvLogChannels.h"
+
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemGlobals.h"
+#include "GameFramework/Character.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(AbilityTask_PlayMontageAndWaitForEvent)
+
+UAbilityTask_PlayMontageAndWaitForEvent::UAbilityTask_PlayMontageAndWaitForEvent(const FObjectInitializer& ObjInitializer)
+	: Super(ObjInitializer)
+{
+}
+
+void UAbilityTask_PlayMontageAndWaitForEvent::Activate()
+{
+	if (Ability == nullptr)
+	{
+		return;
+	}
+
+	bool bPlayedMontage = false;
+
+	if (AbilitySystemComponent.IsValid())
+	{
+		const FGameplayAbilityActorInfo* ActorInfo = Ability->GetCurrentActorInfo();
+		
+		if (UAnimInstance* AnimInstance = ActorInfo->GetAnimInstance())
+		{
+			EventHandle = AbilitySystemComponent->AddGameplayEventTagContainerDelegate(
+				EventTags, FGameplayEventTagMulticastDelegate::FDelegate::CreateUObject(this, &ThisClass::OnGameplayEvent));
+
+			if (AbilitySystemComponent->PlayMontage(Ability, Ability->GetCurrentActivationInfo(), MontageToPlay, Rate, StartSection) > 0.0f)
+			{
+				if (!ShouldBroadcastAbilityTaskDelegates())
+				{
+					return;
+				}
+
+				CancelledHandle = Ability->OnGameplayAbilityCancelled.AddUObject(this, &ThisClass::OnAbilityCancelled);
+
+				BlendingOutDelegate.BindUObject(this, &ThisClass::OnMontageBlendingOut);
+				AnimInstance->Montage_SetBlendingOutDelegate(BlendingOutDelegate, MontageToPlay);
+
+				MontageEndedDelegate.BindUObject(this, &ThisClass::OnMontageEnded);
+				AnimInstance->Montage_SetEndDelegate(MontageEndedDelegate, MontageToPlay);
+
+				if (ACharacter* Character = Cast<ACharacter>(GetAvatarActor()))
+				{
+					Character->SetAnimRootMotionTranslationScale(AnimRootMotionTranslationScale);
+				}
+				
+				bPlayedMontage = true;
+			}
+		}
+		else
+		{
+			UE_LOG(LogMvAbilitySystem, Warning, TEXT("UAbilityTask_PlayMontageAndWaitForEvent::Activate(): Failed to call PlayMontage!"))
+		}
+	}
+	else
+	{
+		UE_LOG(LogMvAbilitySystem, Error, TEXT("UAbilityTask_PlayMontageAndWaitForEvent::Activate(): Invalid AbilitySystem!"))
+	}
+
+	if (!bPlayedMontage)
+	{
+		UE_LOG(LogMvAbilitySystem, Error, TEXT("UAbilityTask_PlayMontageAndWaitForEvent::Activate(): Called in Ability [%s] failed to play montage [%s]; Task Instance Name [%s]."),
+			*Ability->GetName(), *GetNameSafe(MontageToPlay), *InstanceName.ToString());
+
+		if (ShouldBroadcastAbilityTaskDelegates())
+		{
+			OnCancelled.Broadcast(FGameplayTag(), FGameplayEventData());
+		}
+	}
+
+	SetWaitingOnAvatar();
+}
+
+void UAbilityTask_PlayMontageAndWaitForEvent::ExternalCancel()
+{
+	check(AbilitySystemComponent.IsValid());
+
+	OnAbilityCancelled();
+	
+	Super::ExternalCancel();
+}
+
+FString UAbilityTask_PlayMontageAndWaitForEvent::GetDebugString() const
+{
+	UAnimMontage* PlayingMontage = nullptr;
+
+	if (Ability)
+	{
+		const FGameplayAbilityActorInfo* ActorInfo = Ability->GetCurrentActorInfo();
+		UAnimInstance* AnimInstance = ActorInfo->GetAnimInstance();
+
+		if (AnimInstance)
+		{
+			PlayingMontage = AnimInstance->Montage_IsActive(MontageToPlay) ? MontageToPlay : AnimInstance->GetCurrentActiveMontage();
+		}
+	}
+
+	return FString::Printf(TEXT("PlayMontageAndWaitForEvent. MontageToPlay: %s  (Currently Playing): %s"), *GetNameSafe(MontageToPlay), *GetNameSafe(PlayingMontage));
+}
+
+void UAbilityTask_PlayMontageAndWaitForEvent::OnDestroy(bool AbilityEnded)
+{
+	if (Ability)
+	{
+		Ability->OnGameplayAbilityCancelled.Remove(CancelledHandle);
+		if (AbilityEnded && bStopWhenAbilityEnds)
+		{
+			StopPlayingMontage();
+		}
+	}
+
+	if (AbilitySystemComponent.IsValid())
+	{
+		AbilitySystemComponent->RemoveGameplayEventTagContainerDelegate(EventTags, EventHandle);
+	}
+	
+	Super::OnDestroy(AbilityEnded);
+}
+
+UAbilityTask_PlayMontageAndWaitForEvent* UAbilityTask_PlayMontageAndWaitForEvent::PlayMontageAndWaitForEvent(
+	UMvGameplayAbility* OwningAbility,
+	FName TaskInstanceName,
+	UAnimMontage* MontageToPlay,
+	FGameplayTagContainer EventTags,
+	float Rate,
+	FName StartSection,
+	bool bStopWhenAbilityEnds,
+	float AnimRootMotionTranslationScale)
+{
+	UAbilitySystemGlobals::NonShipping_ApplyGlobalAbilityScaler_Rate(Rate);
+
+	UAbilityTask_PlayMontageAndWaitForEvent* Task = NewAbilityTask<UAbilityTask_PlayMontageAndWaitForEvent>(OwningAbility, TaskInstanceName);
+	Task->MontageToPlay = MontageToPlay;
+	Task->EventTags = EventTags;
+	Task->Rate = Rate;
+	Task->StartSection = StartSection;
+	Task->AnimRootMotionTranslationScale = AnimRootMotionTranslationScale;
+	Task->bStopWhenAbilityEnds = bStopWhenAbilityEnds;
+
+	return Task;
+}
+
+bool UAbilityTask_PlayMontageAndWaitForEvent::StopPlayingMontage()
+{
+	const FGameplayAbilityActorInfo* ActorInfo = Ability->GetCurrentActorInfo();
+	if (!ActorInfo)
+	{
+		return false;
+	}
+
+	UAnimInstance* AnimInstance = ActorInfo->GetAnimInstance();
+	if (!AnimInstance)
+	{
+		return false;
+	}
+
+	if (AbilitySystemComponent.IsValid() && Ability)
+	{
+		if (AbilitySystemComponent->GetAnimatingAbility() == Ability
+			&& AbilitySystemComponent->GetCurrentMontage() == MontageToPlay)
+		{
+			FAnimMontageInstance* MontageInstance = AnimInstance->GetActiveInstanceForMontage(MontageToPlay);
+			if (MontageInstance)
+			{
+				MontageInstance->OnMontageBlendingOutStarted.Unbind();
+				MontageInstance->OnMontageEnded.Unbind();
+			}
+
+			AbilitySystemComponent->CurrentMontageStop();
+
+			return true;
+		}
+	}
+
+	return false;
+}
+
+void UAbilityTask_PlayMontageAndWaitForEvent::OnMontageBlendingOut(UAnimMontage* Montage, bool bInterrupted)
+{
+	if (Ability && Ability->GetCurrentMontage() == MontageToPlay)
+	{
+		if (Montage == MontageToPlay)
+		{
+			AbilitySystemComponent->ClearAnimatingAbility(Ability);
+			
+			if (ACharacter* Character = Cast<ACharacter>(GetAvatarActor()))
+			{
+				Character->SetAnimRootMotionTranslationScale();
+			}
+		}
+	}
+
+	if (bInterrupted)
+	{
+		if (ShouldBroadcastAbilityTaskDelegates())
+		{
+			OnInterrupted.Broadcast(FGameplayTag(), FGameplayEventData());
+		}
+	}
+	else
+	{
+		if (ShouldBroadcastAbilityTaskDelegates())
+		{
+			OnBlendOut.Broadcast(FGameplayTag(), FGameplayEventData());
+		}
+	}
+}
+
+void UAbilityTask_PlayMontageAndWaitForEvent::OnAbilityCancelled()
+{
+	if (StopPlayingMontage())
+	{
+		if (ShouldBroadcastAbilityTaskDelegates())
+		{
+			OnCancelled.Broadcast(FGameplayTag(), FGameplayEventData());
+		}
+	}
+}
+
+void UAbilityTask_PlayMontageAndWaitForEvent::OnMontageEnded(UAnimMontage* Montage, bool bInterrupted)
+{
+	if (!bInterrupted)
+	{
+		if (ShouldBroadcastAbilityTaskDelegates())
+		{
+			OnCompleted.Broadcast(FGameplayTag(), FGameplayEventData());
+		}
+	}
+
+	EndTask();
+}
+
+void UAbilityTask_PlayMontageAndWaitForEvent::OnGameplayEvent(FGameplayTag EventTag, const FGameplayEventData* Payload)
+{
+	if (ShouldBroadcastAbilityTaskDelegates())
+	{
+		FGameplayEventData TempData = *Payload;
+		TempData.EventTag = EventTag;
+		OnEventReceived.Broadcast(EventTag, TempData);
+	}
+}
---
+++ Source/MvAbilitySystem/Public/AbilitySystem/Tasks/AbilityTask_PlayMontageAndWaitForEvent.h
@@ -0,0 +1,86 @@
+﻿// Copyright Kyrylo Zaverukha. All Rights Reserved.
+
+#pragma once
+
+#include "Abilities/Tasks/AbilityTask.h"
+#include "AbilityTask_PlayMontageAndWaitForEvent.generated.h"
+
+class UMvGameplayAbility;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FMvPlayMontageAndWaitForEventDelegate, FGameplayTag, EventTag, FGameplayEventData, EventData);
+
+UCLASS()
+class UAbilityTask_PlayMontageAndWaitForEvent : public UAbilityTask
+{
+	GENERATED_BODY()
+	
+public:
+	UAbilityTask_PlayMontageAndWaitForEvent(const FObjectInitializer& ObjInitializer = FObjectInitializer::Get());
+
+	virtual void Activate() override;
+	virtual void ExternalCancel() override;
+	virtual FString GetDebugString() const override;
+	virtual void OnDestroy(bool AbilityEnded) override;
+
+	// The montage completely finished playing.
+	UPROPERTY(BlueprintAssignable)
+	FMvPlayMontageAndWaitForEventDelegate OnCompleted;
+
+	// The montage started blending out.
+	UPROPERTY(BlueprintAssignable)
+	FMvPlayMontageAndWaitForEventDelegate OnBlendOut;
+
+	// The montage was interrupted.
+	UPROPERTY(BlueprintAssignable)
+	FMvPlayMontageAndWaitForEventDelegate OnInterrupted;
+
+	// The ability task was explicitly cancelled by another ability.
+	UPROPERTY(BlueprintAssignable)
+	FMvPlayMontageAndWaitForEventDelegate OnCancelled;
+
+	// One of the triggering gameplay events happened.
+	UPROPERTY(BlueprintAssignable)
+	FMvPlayMontageAndWaitForEventDelegate OnEventReceived;
+
+	UFUNCTION(BlueprintCallable, Category = "MVAS|Tasks")
+	static UAbilityTask_PlayMontageAndWaitForEvent* PlayMontageAndWaitForEvent(
+		UMvGameplayAbility* OwningAbility,
+		FName TaskInstanceName,
+		UAnimMontage* MontageToPlay,
+		FGameplayTagContainer EventTags,
+		float Rate = 1.0f,
+		FName StartSection = NAME_None,
+		bool bStopWhenAbilityEnds = true,
+		float AnimRootMotionTranslationScale = 1.0);
+
+private:
+	UPROPERTY()
+	UAnimMontage* MontageToPlay;
+
+	UPROPERTY()
+	FGameplayTagContainer EventTags;
+	
+	UPROPERTY()
+	float Rate;
+
+	UPROPERTY()
+	FName StartSection;
+
+	UPROPERTY()
+	float AnimRootMotionTranslationScale;
+
+	UPROPERTY()
+	bool bStopWhenAbilityEnds;
+	
+	bool StopPlayingMontage();
+
+	void OnMontageBlendingOut(UAnimMontage* Montage, bool bInterrupted);
+	void OnAbilityCancelled();
+	void OnMontageEnded(UAnimMontage* Montage, bool bInterrupted);
+	void OnGameplayEvent(FGameplayTag EventTag, const FGameplayEventData* Payload);
+
+	FOnMontageBlendingOutStarted BlendingOutDelegate;
+	FOnMontageEnded MontageEndedDelegate;
+	FDelegateHandle CancelledHandle;
+	FDelegateHandle EventHandle;
+};
